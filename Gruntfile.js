1f480492144c20f56e092844918fac963ff45dcd
lasanthasri <lasantha@cxagroup.com>
2015-12-24 06:42:18 +0800

    added live reload with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            scripts: {
+                files: ['*.*'],
+                options: {
+                    livereload: true,
+                }
+            }
+        }
+    });
+
+    //Load live reload plugin
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+
+};
