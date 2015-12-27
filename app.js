2616893358549a86f165a0ea6928d9697140676a
lasanthasri <lasantha@cxagroup.com>
2015-12-27 15:00:07 +0800

    add pages

---
+++ app.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('#content-head nav a').each(function (index, item) {
+        if (item.href == window.location.href) {
+            $(item).addClass('active').siblings().removeClass('active');
+        }
+        if (window.location.pathname == '/' && index == 0) {
+            $(item).addClass('active').siblings().removeClass('active');
+        }
+    });
+});
