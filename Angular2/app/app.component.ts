2553ca20b6e9583cc256be924325b88e53a4a945
DESKTOP-I5E7U99\Lasantha <lasantha@cxagroup.com>
2016-09-18 21:10:21 +0800

    changed the build folder for typescript

--- Angular2/app/app.component.ts
+++ Angular2/app/app.component.ts
@@ -1,6 +1,6 @@
 ﻿import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>once again...</h1>'
 })
 export class AppComponent { }
